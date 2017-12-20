7141ddc42ca17bfcc7415471f6a36af89aa4688b
Lars <larsfinlay@googlemail.com>
2017-12-20 07:52:41 +0000

    Temperatures dependant on Power Savingmode

--- spec/Thermostat_spec.js
+++ spec/Thermostat_spec.js
@@ -2,7 +2,7 @@ describe("Thermostat", function() {
 
   'use strict';
 
-  var testThermostat = new Thermostat('model00')
+  var testThermostat
   var spyThermostat
 
   // spyThermostat = {
@@ -14,10 +14,14 @@ describe("Thermostat", function() {
   //     }
   //   }
 
+  beforeEach(function() {
+    testThermostat = new Thermostat('model00');
+  });
+
   describe("defaultTemp", function() {
 
-    it("Should default to 20", function() {
-      expect(testThermostat.displayTemp()).toEqual('Current temperature: 20')
+    it("Should default to 20°C", function() {
+      expect(testThermostat.displayTemp()).toEqual('Current temperature: 20°C')
     });
 
   });
@@ -25,15 +29,59 @@ describe("Thermostat", function() {
   describe("increaseTemp", function() {
 
     it("Should be able to increase the temperatue", function() {
-      expect(testThermostat.displayTemp()).toEqual('Current temperature: 20');
-      testThermostat._upTemp(1);
-      expect(testThermostat.displayTemp()).toEqual('Current temperature: 21');
+      expect(testThermostat.displayTemp()).toEqual('Current temperature: 20°C');
+      testThermostat.pressUpTemp(1);
+      expect(testThermostat.displayTemp()).toEqual('Current temperature: 21°C');
     });
 
     it("Should be able to increase the temperatue by a chosen amount", function() {
-      expect(testThermostat.displayTemp()).toEqual('Current temperature: 20');
-      testThermostat._upTemp(1);
-      expect(testThermostat.displayTemp()).toEqual('Current temperature: 21');
+      expect(testThermostat.displayTemp()).toEqual('Current temperature: 20°C');
+      testThermostat.pressUpTemp(3);
+      expect(testThermostat.displayTemp()).toEqual('Current temperature: 23°C');
+    });
+
+  });
+
+  describe("decreaseTemp", function() {
+
+    it("Should be able to decrease the temperatue", function() {
+      expect(testThermostat.displayTemp()).toEqual('Current temperature: 20°C');
+      testThermostat.pressDownTemp(1);
+      expect(testThermostat.displayTemp()).toEqual('Current temperature: 19°C');
+    });
+
+    it("Should be able to decrease the temperatue by a chosen amount", function() {
+      expect(testThermostat.displayTemp()).toEqual('Current temperature: 20°C');
+      testThermostat.pressDownTemp(3);
+      expect(testThermostat.displayTemp()).toEqual('Current temperature: 17°C');
+    });
+
+  });
+
+  describe("minimumTemp", function() {
+
+    it("The minimum temperature should be 10°C", function() {
+      expect(testThermostat._minTemp).toEqual(10);
+    });
+
+    it("Should give an error when trying to decrease the temperature below 10°C", function() {
+      expect(function() {testThermostat.pressDownTemp(100)}).toThrow(new Error("This change breaches the range of this thermostat"));
+    });
+
+  });
+
+  describe("maxTemp", function() {
+
+    it("Max temperature should be 25°C when on power saving mode", function() {
+      expect(testThermostat._maxTemp).toEqual(25);
+    });
+
+  });
+
+  describe("powerSavingMode", function() {
+
+    it("Power saving mode is on by default", function() {
+      expect(testThermostat.displayPowerMode()).toEqual('Power-Save-Mode: ON');
     });
 
   });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,18 +1,41 @@
 var Thermostat = function(make) {
   this.make = make
+  this._powerSavingMode = true
+  this._minTemp = 10
+  this._maxTemp = (this._powerSavingMode ? 25 : 32)
   this._temperature = 20
-  this._upTemp = function(increment) {
-    this._temperature += increment 
+  this._altTemp = function(increment) {
+    if ((this._temperature + increment) < this._minTemp) {
+      throw(new Error("This change breaches the range of this thermostat"))
+    }
+    this._temperature += increment
   };
 };
 
 Thermostat.prototype.displayTemp = function() {
-  return ('Current temperature: '+this._temperature)
+  return ('Current temperature: '+this._temperature+'°C');
+};
+
+Thermostat.prototype.displayPowerMode = function() {
+  var state = (this._powerSavingMode ? 'ON' : 'OFF')
+  // if(this._powerSavingMode) {
+  //   var state = 'ON'
+  // } else {
+  //   var state = 'OFF'
+  // }
+  return ('Power-Save-Mode: '+state);
 };
 
 Thermostat.prototype.pressUpTemp = function(increment) {
   if (increment === undefined) {
     increment = 1
   };
-  this._upTemp(increment)
+  this._altTemp(increment);
+};
+
+Thermostat.prototype.pressDownTemp = function(increment) {
+  if (increment === undefined) {
+    increment = 1
+  };
+  this._altTemp(increment*(-1));
 };
